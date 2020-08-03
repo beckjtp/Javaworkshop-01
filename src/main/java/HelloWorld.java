71c527fa74d6dba5140153706fcac58f60f554c0
Jitrin Panitdirake <jitrinpan@mathregit.group>
2020-08-03 11:39:34 +0700

    First Test complete

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    public String seyHi() {
+        return "Hello World";
+    }
+
+    public String seyHiPui() {
+        return "Hello Pui";
+    }
+
+    public String seyHiSomkiat() {
+        return "Hello Somkiat";
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test
+    public void sayHi_should_return_Hello_World(){
+        HelloWorld helloWorld = new HelloWorld();
+        String result = helloWorld.seyHi();
+        assertEquals("Hello World",result);
+    }
+
+    @Test
+    public void sayHi_with_somkiat_should_return_Hello_World(){
+        HelloWorld helloWorld = new HelloWorld();
+        String result = helloWorld.seyHiSomkiat();
+        assertEquals("Hello Somkiat",result);
+    }
+
+    @Test
+    public void sayHi_with_pui_should_return_Hello_World(){
+        HelloWorld helloWorld = new HelloWorld();
+        String result = helloWorld.seyHiPui();
+        assertEquals("Hello Pui",result);
+    }
+}
